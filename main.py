83bd554a32569d88bdc94ef1cc26500da4b99ba4
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-08 22:52:35 -0700

    Milo Dominguez

--- main.py
+++ main.py
@@ -8,5 +8,17 @@ idxlst = []
 # Make your Code
 # ******************************
 
+for i in range(len(words)):
+    k = 0
+    for j in range(len(words[i])):
+        if are[k] == words[i][j]:
+            k += 1
+        if k == len(are):
+            break
+    if k == len(are):
+        idxlst.append(words[i])
+
+print(words)
+
 # print the words that has 'a', 'r', 'e' in sequence
 
